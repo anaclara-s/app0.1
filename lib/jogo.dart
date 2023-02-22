dcfcc5bfcc5f91e31b07985edc27b5e6cf6c116b
Ana <anaclarasantos.amorin@gmail.com>
2023-02-22 20:45:35 -0300

    first commit

---
+++ lib/jogo.dart
@@ -0,0 +1,106 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class paginaJogo extends StatefulWidget {
+  @override
+  State<paginaJogo> createState() => _paginaJogo();
+}
+
+class _paginaJogo extends State<paginaJogo> {
+  List opcoes = ['pedra', 'papel', 'tesoura'];
+  String mensagem = 'Escolha uma opção: ';
+  // String escAleatoria = 'imagens/selo.png';
+  var imagemApp = AssetImage('imagens/selo.png');
+
+  void jogada(String escolha) {
+    int O = Random().nextInt(opcoes.length);
+    String aleatorio = opcoes[O];
+    // this.escAleatoria = '' + aleatorio + '.png';
+    this.imagemApp = AssetImage('imagens/$aleatorio.png');
+
+    print('click: ' + escolha + ', Random: ' + aleatorio);
+
+    if ( //VITORIA USUARIO
+        (escolha == 'pedra' && aleatorio == 'tesoura') ||
+            (escolha == 'papel' && aleatorio == 'pedra') ||
+            (escolha == 'tesoura' && aleatorio == 'papel')) {
+      setState(() {
+        this.mensagem = 'VITORIA ✧⁠◝⁠(⁠⁰⁠▿⁠⁰⁠)⁠◜⁠✧';
+      });
+    } else if ( //DERROTA USUARIO
+        (escolha == 'pedra' && aleatorio == 'papel') ||
+            (escolha == 'papel' && aleatorio == 'tesoura') ||
+            (escolha == 'tesoura' && aleatorio == 'pedra')) {
+      setState(() {
+        this.mensagem = 'DERROTA (⁠╯⁠ರ⁠⁠~⁠⁠ರ⁠)⁠╯⁠︵⁠⁠┻⁠━⁠┻';
+      });
+    } else {
+      //EMPATE
+      setState(() {
+        this.mensagem = 'EMPATE (ㆆ_ㆆ)';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pedra, Papel e Tesoura'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Text(
+                'Computador escolheu',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            //Image.asset(escAleatoria, height: 250),
+            Image(image: imagemApp, height: 250),
+            Padding(
+              padding: EdgeInsets.only(bottom: 30),
+              child: Text(
+                mensagem,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () => jogada('pedra'),
+                  child: Image.asset('imagens/pedra.png', height: 80),
+                ),
+                GestureDetector(
+                  onTap: () => jogada('papel'),
+                  child: Image.asset(
+                    'imagens/papel.png',
+                    height: 80,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => jogada('tesoura'),
+                  child: Image.asset(
+                    'imagens/tesoura.png',
+                    height: 80,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'jogo.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'PEDRA,PAPEL E TESOURA',
+      debugShowCheckedModeBanner: false,
+      home: paginaJogo(),
+    ),
+  );
+}
